ba270073413d617141fe420c431ef1bf0cb73446
Kevin Nguyen <nguyenkevin225@gmail.com>
2020-02-17 13:43:09 -0800

    searching + stretch completed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,18 +1,19 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
+    if len(arr) == 0:
+        return []
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+        smallest_index = i
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
-
-
+        for k in range(i, len(arr)):              #mistake - len(arr) NOT len(arr)-1
+            if arr[k] < arr[smallest_index]:
+                smallest_index = k
         # TO-DO: swap
-
-
+        item = arr[smallest_index]
+        arr[smallest_index] = arr[i]
+        arr[i] = item
 
 
     return arr
@@ -20,11 +21,41 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    if len(arr) == 0:
+        return []
+
+    for i in range(0, len(arr)-2):
+        for k in range(0, len(arr)-1):
+            if arr[k] > arr[k+1]:
+                item = arr[k+1]
+                arr[k+1] = arr[k]
+                arr[k] = item
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
+    maximum = -1
+    for i in range(0, len(arr)-1):
+        if arr[i] > maximum:
+            maximum = arr[i]
+    
+    newArr = []
+    for i in range(0, maximum):
+        newArr[i] = 0
+    
+    for i in range(0, len(arr)-1):
+        newArr[arr[i]] += 1
+
+    # for i in range(0, len(arr)-1):
+
+
 
     return arr
+
+# sorting algorithm 
+# counting sort = counting occurrances
+# counting each item for its unique occurrances 
+# maximum = number of elements from 0 to element
+# make an array size maximum to track occurrances 
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,9 +1,14 @@
 # STRETCH: implement Linear Search				
 def linear_search(arr, target):
-  
-  # TO-DO: add missing code
+  if len(arr) == 0:
+    return -1
 
-   return -1   # not found
+  # TO-DO: add missing code
+  for i in range(0, len(arr)):
+    if target == arr[i]:
+      return i 
+
+  return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
@@ -16,15 +21,32 @@ def binary_search(arr, target):
   high = len(arr)-1
 
   # TO-DO: add missing code
-
+  while high > low:
+    midpoint = high + low // 2
+    if target == arr[midpoint]:
+      return midpoint 
+    elif arr[midpoint] > target:
+      high = midpoint-1
+    elif arr[midpoint] < target:
+      low = midpoint + 1
   return -1 # not found
 
 
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
-  
   middle = (low+high)//2
 
   if len(arr) == 0:
     return -1 # array empty
   # TO-DO: add missing if/else statements, recursive calls
+  if low > high:
+    return middle
+  if target == arr[middle]:
+    return middle 
+  elif target > arr[middle]:
+    low = middle+1        #make sure you are changing the lower interval when target is higher than midpoint
+    return binary_search_recursive(arr, target, low, high)
+  elif target < arr[middle]:
+    high = middle-1      #make sure you are changing the higher interval when target is lower than midpoint 
+    return binary_search_recursive(arr, target, low, high)
+
